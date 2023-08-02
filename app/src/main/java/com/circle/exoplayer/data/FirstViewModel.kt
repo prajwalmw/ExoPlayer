e0a4407789edb696da2d96d7eb05f619fe1f4135
Prajwal <prajwalwaingankar@gmail.com>
2023-08-02 21:53:39 +0530

    Initial commit

---
+++ app/src/main/java/com/circle/exoplayer/data/FirstViewModel.kt
@@ -0,0 +1,11 @@
+package com.circle.exoplayer.data
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class FirstViewModel : ViewModel() {
+
+    var editField = MutableLiveData<String>()
+
+
+}
---
+++ app/src/main/java/com/circle/exoplayer/ui/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.circle.exoplayer.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.circle.exoplayer.R
+import com.circle.exoplayer.data.FirstViewModel
+import com.circle.exoplayer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var viewmodel : FirstViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewmodel = ViewModelProvider(this).get(FirstViewModel::class.java)
+        binding.vm = viewmodel
+        binding.lifecycleOwner = this
+    }
+}
